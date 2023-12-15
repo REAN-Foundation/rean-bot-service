d1442c2228cc4d12ce42a9faca1d49d637dfab1e
Kiran Kharade <kiran.kharade@reanfoundation.org>
2023-12-15 22:15:36 +0530

    Removed pre and post processors

--- src/message.postprocessor/message.postprocessor.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from "../domain.types/message";
-
-export class MessagePostprocessor {
-
-    static async process(message: Message): Promise<Message> {
-        return message;
-    }
-
-}
--- src/message.preprocessor/message.preprocessor.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from "../domain.types/message";
-
-export class MessagePreprocessor {
-
-    static async process(message: Message): Promise<Message> {
-        return message;
-    }
-
-}
