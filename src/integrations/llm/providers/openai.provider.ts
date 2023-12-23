e18ca5ff5f08346c0c2639bd1024597924822f49
Kiran Kharade <kiran.kharade@reanfoundation.org>
2023-12-23 15:27:56 +0530

    Updated openai provider

--- src/integrations/llm/providers/openai.provider.ts
+++ src/integrations/llm/providers/openai.provider.ts
@@ -1,16 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { inject } from "tsyringe";
 import { LanguageCode } from "../../../domain.types/language";
 import { ILLMServiceProvider } from "../llm.provider.interface";
+import OpenAI from 'openai';
+import { TenantEnvironmentProvider } from "../../../auth/tenant.environment/tenant.environment.provider";
 
+////////////////////////////////////////////////////////////////////////////////
 export class OpenAIProvider implements ILLMServiceProvider {
 
-    // private _openai: OpenAI;
+    private _openai: OpenAI;
 
-    // constructor(
-    //     @inject('OpenAI') openai?: OpenAI,
-    // ) {
-    //     this._openai = openai;
-    // }
+    constructor(
+        @inject('TenantEnvironmentProvider') private _tenantEnvProvider?: TenantEnvironmentProvider,
+    ) {
+        const apiKey = this._tenantEnvProvider.getTenantEnvironmentVariable("OPENAI_API_KEY");
+        //process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+        this._openai = new OpenAI({
+            apiKey : apiKey
+        });
+
+    }
 
     public translate = async (targetLanguage: LanguageCode, text: string): Promise<string> => {
         return text;
